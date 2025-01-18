8e4bad54bbc5e70ab6a720e93ac57fd4cf80c291
BiancaElena13 <avasiloaiebiancaelena@yahoo.com>
2025-01-18 12:32:13 +0200

    Calculatorul meu

---
+++ main.py
@@ -0,0 +1,9 @@
+import calculator
+
+a=int(input("introduceti a:"))
+b=int(input("introduceti b:"))
+
+suma=calculator.adunare(a,b)
+diferenta=calculator.scadere(a,b)
+
+print(f"Suma este:{suma}, Diferenta este {diferenta}")
